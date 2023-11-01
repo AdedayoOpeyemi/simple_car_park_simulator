3546c37857a99a3fe3952c4e7543a27c4fb843e4
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-11-01 20:08:28 +0100

    main function and initial display of menu

---
+++ cmd_interface.py
@@ -0,0 +1,18 @@
+
+def main():
+    print("Welcome to the Best and safest car park in the world")
+    display_menu()
+
+
+    
+
+def display_menu():
+    print("You can choose of of the following options from the menu below:")
+    print("1. Enter the car park")
+    print("2. Exit the car park")
+    print("3. Query parking record by ticket number")
+    print("4. Quit")
+    print("0. Exit")
+
+if __name__ == "__main__":
+    main()
